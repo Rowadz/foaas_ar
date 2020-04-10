65b1b531d3b8737eba1822c1639deb93d5651ab1
Rowad <rowadwo@gmail.com>
2020-04-11 00:28:06 +0300

    deploy

---
+++ precache-manifest.984a38903526037c4e54b78287e60039.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac002874277deb582c8db0dec0398a47",
+    "url": "/index.html"
+  },
+  {
+    "revision": "557bb384046f19de31a8",
+    "url": "/static/css/2.4af70dfe.chunk.css"
+  },
+  {
+    "revision": "27b26e817472b45244b7",
+    "url": "/static/css/main.9ab195c6.chunk.css"
+  },
+  {
+    "revision": "557bb384046f19de31a8",
+    "url": "/static/js/2.4ee8f1ec.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.4ee8f1ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27b26e817472b45244b7",
+    "url": "/static/js/main.1519d8a8.chunk.js"
+  },
+  {
+    "revision": "34d3a8c431fc289ef5c3",
+    "url": "/static/js/runtime-main.1908e5cb.js"
+  }
+]);
