11c4991b452d21d216857a1a0295d24fc06feb65
Rowad <rowadwo@gmail.com>
2020-04-10 23:32:54 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
+import { PageHeader } from 'antd'
+import { Row, Col } from 'antd'
+import { Card } from 'antd'
+import { Input } from 'antd'
+import { AlertTwoTone } from '@ant-design/icons'
+const queryString = require('query-string')
 
 function App() {
+  const [state, setState] = useState({
+    query: queryString.parse(window.location.search),
+    name: null
+  })
+
+  const weHaveQuery = Object.keys(state.query).length > 0
+  console.log(weHaveQuery)
+  const nameChange = (e) => {
+    setState({ ...state, name: e.target.value })
+  }
+
+  const navigate = () => {
+    const { pathname } = window.location
+    const { name } = state
+    window.location.href = `${pathname}?` + queryString.stringify({ name })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main>
+      <PageHeader
+        ghost="true"
+        style={{ background: '#0F0F0F' }}
+        backIcon="false"
+        title="تحلسـ \ يـ طيزي كخدمة"
+        // subTitle="This is a subtitle"
+      />
+      <Row>
+        <Col span={24} style={{ padding: '50px 50px' }}>
+          <Card
+            title="اكتبـ \ يـ الاسم"
+            actions={[<AlertTwoTone key="setting" onClick={navigate} />]}
+          >
+            <Input
+              size="large"
+              value={state.name}
+              placeholder="اسم"
+              onChange={nameChange}
+            />
+          </Card>
+        </Col>
+      </Row>
+    </main>
+  )
 }
 
-export default App;
+export default App
